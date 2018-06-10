7beb998e8c81ed2eebdcc9fe75f1dc657c4f454d
Armando <arfega.anisgaro@gmail.com>
2018-06-10 23:23:42 +0200

    Terminado adapter

--- app/src/main/java/com/example/armando/userlist/presentation/userList/UserListActivity.kt
+++ app/src/main/java/com/example/armando/userlist/presentation/userList/UserListActivity.kt
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.armando.userlist.R
 
-class MainActivity : AppCompatActivity() {
+class UserListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/example/armando/userlist/presentation/userList/UserListAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.armando.userlist.presentation.userList
+
+import android.support.v7.recyclerview.extensions.ListAdapter
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bumptech.glide.Glide
+
+import com.example.armando.userlist.R
+import com.example.armando.userlist.data.model.UserEntity
+import com.example.armando.userlist.data.model.UserEntityDiff
+import kotlinx.android.synthetic.main.item_user.view.*
+
+class UserListAdapter : ListAdapter<UserEntity,UserListAdapter.UserListVH>(UserEntityDiff())  {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserListVH {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_user,parent,false)
+        return  UserListVH(view)
+    }
+
+    override fun onBindViewHolder(holder: UserListVH, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+
+    inner class UserListVH(view:View) : RecyclerView.ViewHolder(view){
+            fun bind(userEntity: UserEntity){
+                with(itemView){
+                    user_name.text = userEntity.name
+                    //Cargamos imagen con Glide
+                    Glide.with(user_avatar)
+                            .load(userEntity.avatarUrl)
+                            .into(user_avatar)
+                }
+            }
+        }
+
+}
